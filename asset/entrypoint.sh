4516abd05408cf75acc70f4e53285145d34db3d6
Hong-Ming <ming-chou@MingChou-MacBookAir.local>
2020-09-04 00:18:17 +0800

    UPD: Finish Docker Image and Docker Compose

--- asset/entrypoint.sh
+++ asset/entrypoint.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-tail -f /etc/hosts
+
+test -f /etc/named.rfc1912.zones || yum install -y bind bind-libs bind-chroot bind-utils
+test -f /etc/named.root.key || yum install -y bind bind-libs bind-chroot bind-utils
+test -f /etc/named.iscdlv.key || yum install -y bind bind-libs bind-chroot bind-utils
+test -f /var/named/named.ca || cp -r /var/named-default/* /var/named
+test -f /var/named/named.localhost || cp -r /var/named-default/* /var/named
+test -f /var/named/named.loopback || cp -r /var/named-default/* /var/named
+test -f /var/named/named.empty || cp -r /var/named-default/* /var/named
 
 
-NAMEDCONF=/etc/named.conf
+test -z $NAMED_CONF && NAMED_CONF=/etc/named.conf
+test -z $LOG_LEVEL && LOG_LEVEL=6
 
 /bin/bash -c 'if [ ! "$DISABLE_ZONE_CHECKING" == "yes" ]; then /usr/sbin/named-checkconf -z "$NAMEDCONF"; else echo "Checking of zone files is disabled"; fi'
-/usr/sbin/named -u named -c ${NAMEDCONF}
+/usr/sbin/named -u named -c ${NAMED_CONF} -d ${LOG_LEVEL}
+
+
+tail -f /var/named/data/named.run
 
 
